581c32c2496ddd46a3d6392d2f464d2825e74eaa
kyleteloser <kylebethke123@gmail.com>
2016-12-27 07:16:31 -0600

    Add files via upload

---
+++ Events.py
@@ -0,0 +1,16 @@
+#Zombie horde
+#spoil crop
+#Contiamed water
+#Raiders
+#OutBreak
+#A way in
+#Surivor mental
+#Reglion
+# A FEAST
+# Trader max *wink wink
+
+
+#Zombie horde = 1,30 zombies each zombies do steal 1/2 of food and water per zombie if defender has 4 attack 4 zombies are killed :DD
+
+
+
